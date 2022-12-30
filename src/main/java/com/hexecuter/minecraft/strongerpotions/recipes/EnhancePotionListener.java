497b23049f38b76f6105926295322a9d4c11e323
unknown <ealvarez2025@gmail.com>
2022-12-30 12:16:31 -0700

    Initial commit Clean start with Spigot configured through Maven

---
+++ src/main/java/com/hexecuter/minecraft/strongerpotions/recipes/EnhancePotionListener.java
@@ -0,0 +1,14 @@
+package com.hexecuter.minecraft.strongerpotions.recipes;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+
+public class EnhancePotionListener implements Listener {
+
+    @EventHandler
+    public void onPrepareAnvilEvent(PrepareAnvilEvent event) {
+
+
+    }
+}
